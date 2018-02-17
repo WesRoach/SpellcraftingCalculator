421bc1cafcf0112b3b48e9b789567e92cfeab584
Aaron Tomason <artomason@gmail.com>
2018-02-17 18:22:26 -0500

    Initial

--- ItemDatabaseDialog.py
+++ ItemDatabaseDialog.py
@@ -2,15 +2,20 @@
 
 from json import load
 
+data = load(open(r'database/ItemDatabase.json'))
+meta = load(open(r'database/ItemDatabaseMetaData.json'))
 
-class ItemDatabase:
-    def __init__(self):
-        self.parseDatabase(r'database/ItemDatabaseMetaData.json')
 
-    def parseDatabase(self, database):
-        keys = load(open(database))
+for item in data['items']:
+    print(
+        'ID:', item['id'],
+        ', Name:', item['name'],
+        ', Realm:', meta['realm'][str(item['realm'])],
+    )
 
-        print(keys)
-
-        for key in keys().items():
-            print(key)
+# for item in data['items']:
+#     for key, value in item.items():
+#         if key in ('realm', 'slot'):
+#             for index in meta[key]:
+#                 if str(value) == index:
+#                     print(meta[key][index])
