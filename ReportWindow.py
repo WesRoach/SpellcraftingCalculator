7287b6869309caf9478f2625ba434d41f3ed3715
Aaron Tomason <artomason@gmail.com>
2018-03-21 19:11:46 -0400

    Initial

--- ReportWindow.py
+++ ReportWindow.py
@@ -37,44 +37,13 @@ class ReportWindow(QDialog, Ui_ReportWindow):
         self.ExportHTMLButton.hide()
         self.ExportPlainTextButton.hide()
 
-        materials = {'Items': {}, 'Jewels': {}, 'Gems': {}, 'Liquids': {}, 'Dusts': {}}
+        materials = {'Items': {}, 'Gems': {}, 'Liquids': {}, 'Dusts': {}}
 
         # GATHER CRAFTABLE AND EQUIPPED ITEMS ...
         for location, item in item_list.items():
             if item.ActiveState != 'Dropped' and item.Equipped != 0:
-                materials['Items'][location] = item
+                pass
 
-        # GATHER JEWELS FOR EACH LOCATION ...
-        for location, item in materials['Items'].items():
-            for slot in item.getSlotList():
-                if slot.getSlotType() == 'Craftable' and slot.getEffectType() != 'Unused':
-                    if location not in materials['Jewels']:
-                        materials['Jewels'][location] = {}
-                    if slot.getGemName(realm) in materials['Jewels'][location]:
-                        materials['Jewels'][location][slot.getGemName(realm)] += 1
-                    else:
-                        materials['Jewels'][location][slot.getGemName(realm)] = 1
-
-        # GATHER ALL THE MATERIALS ...
-        for location, item in materials['Items'].items():
-            for slot in item.getSlotList():
-                if slot.getSlotType() == 'Craftable' and slot.getEffectType() != 'Unused':
-                    for material_type, material_list in slot.getGemMaterials(realm).items():
-                        for material, amount in material_list.items():
-                            if material in materials[material_type]:
-                                materials[material_type][material] += amount
-                            else:
-                                materials[material_type][material] = amount
-
-        # SORT ALL THE LISTS ...
-        for material_type, material_list in materials.items():
-            if material_type in GemMaterialsOrder.keys():
-                keys = GemMaterialsOrder[material_type]
-                material_list = [[x, material_list.get(x)] for x in keys if x in material_list]
-            materials[material_type] = material_list
-
-        # GENERATE THE MATERIALS REPORT ...
-        report = etree.Element('Report')
         for key, value in materials.items():
             print(key, value)
 
@@ -88,3 +57,7 @@ class ReportWindow(QDialog, Ui_ReportWindow):
         report = transform(report)
 
         self.ReportTextBrowser.setHtml(str(report))
+
+# =============================================== #
+#              XML IMPORT AND EXPORT              #
+# =============================================== #
