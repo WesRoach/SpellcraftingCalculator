995349800a7be1c2f4eccfe7bb9ef56e9f4e7b39
artomason <artomason@gmail.com>
2018-12-31 12:31:23 -0500

    Initial

--- MainWindow.py
+++ MainWindow.py
@@ -26,7 +26,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         # BUILD - MAJOR.YEAR.MONTHDAY ...
-        self.Build = "3.18.1230 (Alpha)"
+        self.Build = "3.18.1231 (Alpha)"
 
         font = QFont()
         font.setFamily("Trebuchet MS")
